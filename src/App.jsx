a62c49a78eecc89dc2f200d9bc590939c64f5c71
Mohit Kumar <mohitkumarxm@gmail.com>
2025-02-14 00:27:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-red-500 font-bold text-2xl">I am using tailwind</div>
+    </>
+  );
+}
+
+export default App;
