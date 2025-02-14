c934accfccecc6890a56d63e090cb069833d82cd
Mohit Kumar <mohitkumarxm@gmail.com>
2025-02-14 13:52:40 +0530

    basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import "./App.css";
+import Body from "./components/Body";
 
 function App() {
-  return (
-    <>
-      <div className="text-red-500 font-bold text-2xl">I am using tailwind</div>
-    </>
-  );
+  return <Body />;
 }
 
 export default App;
