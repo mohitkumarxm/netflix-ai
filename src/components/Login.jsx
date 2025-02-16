4e1c0c35f373044ac3bdd1eee8805f5b82d35d3e
Mohit Kumar <mohitkumarxm@gmail.com>
2025-02-16 14:10:48 +0530

    firebase added, form validation added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,34 @@
+import { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidateData } from "../utils/validate";
 
 const Login = () => {
+  const [isSignInForm, setSignInForm] = useState(false);
+  const [errorMessages, setErrorMessages] = useState(null);
+
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const toggleSignInForm = () => {
+    setSignInForm(!isSignInForm);
+  };
+
+  const handleButtonClick = () => {
+    console.log("Email: ", email.current.value);
+    console.log("Password: ", password.current.value);
+
+    const validate = checkValidateData(
+      email.current.value,
+      password.current.value
+    );
+    console.log("validate: ", validate);
+    if (validate) {
+      setErrorMessages(validate);
+    } else {
+      setErrorMessages(null);
+    }
+  };
   return (
     <>
       <div className="">
@@ -15,32 +43,58 @@ const Login = () => {
           <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black/50 max-w-lg mx-auto p-6 rounded-lg">
             <div className=" text-white">
               <h1 className="text-4xl font-bold">
-                Unlimited movies, TV shows and more.
+                {isSignInForm ? "Sign Up" : "Sign In"}
               </h1>
-              <h2 className="text-2xl">Watch anywhere. Cancel anytime.</h2>
-              <p className="text-lg">
-                Ready to watch? Enter your email to create or restart your
-                membership.
-              </p>
             </div>
 
             <div className="">
-              <form>
+              <form onSubmit={(e) => e.preventDefault()}>
+                {isSignInForm && (
+                  <input
+                    ref={name}
+                    type="text"
+                    placeholder="Full Name"
+                    className="p-2 rounded-md w-full border-amber-50 text-white py-4 my-4"
+                  />
+                )}
+
                 <input
+                  ref={email}
                   type="email"
                   placeholder="Email address"
                   className="p-2 rounded-md w-full border-amber-50 text-white py-4 my-4"
                 />
                 <input
+                  ref={password}
                   type="password"
                   placeholder="password"
                   className="p-2 rounded-md w-full border-amber-50 text-white py-4 my-4"
                 />
-                <button className="bg-red-600 text-white p-2 pb-4 font-bold text-2xl w-full rounded-md">
+                {errorMessages && (
+                  <div className="py-4 text-red-600 text-center font-bold">
+                    {errorMessages}
+                  </div>
+                )}
+                <button
+                  className="bg-red-600 text-white p-2 pb-4 font-bold text-2xl w-full rounded-md"
+                  onClick={() => handleButtonClick()}
+                >
                   Get Started
                   <span className="font-bold text-4xl"> &gt;</span>
                 </button>
               </form>
+            </div>
+            <div className=" text-white text-center py-4">
+              {isSignInForm ? "Already have an account? " : "New to Netflix? "}
+
+              <button
+                className=" text-white font-bold cursor-pointer"
+                onClick={() => {
+                  toggleSignInForm();
+                }}
+              >
+                {isSignInForm ? " Sign In now." : " Sign up now."}
+              </button>
             </div>
           </div>
         </div>
---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA8nyt5mL2EdiLvnyZNHzmu0qcRYa2huC0",
+  authDomain: "netflix-ai-21809.firebaseapp.com",
+  projectId: "netflix-ai-21809",
+  storageBucket: "netflix-ai-21809.firebasestorage.app",
+  messagingSenderId: "477743509879",
+  appId: "1:477743509879:web:d775711297ee6bbbb0f172",
+  measurementId: "G-2V8T2VEJH5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
